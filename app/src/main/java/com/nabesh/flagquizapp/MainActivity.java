03ccefcc67d80f4f61c13fe1ff28e56fd7039966
KariukiAbel <abelmuriithi@gmail.com>
2020-03-19 12:18:58 +0300

    Finished the loadNextFlag method.Was quite tideous but I've managed to learn

--- app/src/main/java/com/nabesh/flagquizapp/MainActivity.java
+++ app/src/main/java/com/nabesh/flagquizapp/MainActivity.java
@@ -2,18 +2,25 @@ package com.nabesh.flagquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -127,6 +134,73 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadNextFlag() {
+        //get file name of the next flag and remove it from the list
+        String nextImageName = quizCountryList.remove(0);
+        correctAnswer = nextImageName; //update the correct answer
+        answerTextView.setText("");
 
+        //display the number of current question in the quiz
+        questionNumberTextView.setText(getResources().getString(R.string.question)
+                + " " +(correctAnswers + 1) + " " + getResources().getString(R.string.of) + " 10");
+
+        //extract the region from the next image's name
+        String region = nextImageName.substring(0, nextImageName.indexOf('-'));
+
+        //use AssetManager to load next image from assets folder
+        AssetManager assetManager = getAssets();
+        InputStream stream; //used to read in flag names
+
+        try {
+            //get an inputStream to the asset representing the next flag
+            stream = assetManager.open(region + "/" + nextImageName + ".png");
+
+            //load the asset as a Drawable and display on the flagImageView
+            Drawable flag = Drawable.createFromStream(stream,nextImageName);
+            flagImageView.setImageDrawable(flag);
+
+        } catch (IOException e) {
+//            e.printStackTrace();
+            Log.e(TAG,"Error loading " +nextImageName,e);
+        }
+
+        //Clear all prior answer Buttons from TableRows
+        for (int row = 0; row < buttonTableLayout.getChildCount(); ++row)
+            ((TableRow) buttonTableLayout.getChildAt(row)).removeAllViews();
+
+            Collections.shuffle(fileNameList);
+
+            //put the correct answers to the end of the fileNameList
+            int correct = fileNameList.indexOf(correctAnswer);
+            fileNameList.add(fileNameList.remove(correct));
+
+            //get reference to the LayoutInflater service
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            //add 3,6 or 9 answer buttons based on the value of guessRows
+            for (int row = 0; row < guessRows; row++){
+                TableRow currentTableRow = getTableRow(row);
+
+                //place buttons in currentTableRow
+                for (int column = 0; column < 3; column ++){
+
+                    //inflate guess_button.xml to create new Button
+                    Button newGuessButton = (Button) inflater.inflate(R.layout.guess_button, null);
+
+                    //get country name and set it as newGuessButton's text
+                    String fileName = fileNameList.get((row*3) + column);
+                    newGuessButton.setText(getCountryName(fileName));
+
+                    //register answerButtonListener to respond to Button clicks
+                    newGuessButton.setOnClickListener(newGuessButtonListener);
+                    currentTableRow.addView(newGuessButton);
+                }
+            }
+
+            //randomly replace one button with the correct answer
+        int row = random.nextInt(guessRows);
+            int column = random.nextInt(3);
+            TableRow randomTableRow = getTableRow(row);
+            String countryName = getCountryName(correctAnswer);
+        ((Button)randomTableRow.getChildAt(column)).setText(countryName);
+        }
     }
-}
